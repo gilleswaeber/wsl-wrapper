859a51cfafcbe4759802940a27781a679eb76a2d
Gilles Waeber <moi@gilleswaeber.ch>
2017-05-17 00:34:12 +0200

    Bash executable may be in another folder

--- wsl.bat
+++ wsl.bat
@@ -4,4 +4,4 @@ REM Example: wsl git status
 set wslcmd=%*
 setlocal enabledelayedexpansion
 echo.rm -- "$0";!wslcmd!>wsltemp.sh
-"%~dp0bash" -ic 'sed -i "s/[\r]//g" wsltemp.sh; ./wsltemp.sh'
+bash -ic 'sed -i "s/[\r]//g" wsltemp.sh; ./wsltemp.sh'
